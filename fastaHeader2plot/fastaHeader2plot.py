046ada58324409428f7f0e8d8d79e5b365e6ff44
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2020-08-27 17:40:20 -0300

    Update fastaHeader2plot.py

--- fastaHeader2plot/fastaHeader2plot.py
+++ fastaHeader2plot/fastaHeader2plot.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import string
-from biotext.fastatools import getHeader, fastaread, removePattern, getCons, list2fasta, getSeq
+from biotext.fastatools import getHeader, fastaread, removePattern, getCons, list2fasta, getSeq, fastawrite
 from biotext.aminocode import encodefile, decodetext
 
 # PARAMETERS
