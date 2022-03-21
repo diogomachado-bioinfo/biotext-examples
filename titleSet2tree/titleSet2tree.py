2498ae43aac2dc404030acdbd5af41f3fef5faaf
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2022-03-20 23:14:30 -0300

    Add files via upload

---
+++ titleSet2tree/titleSet2tree.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+AUTHOR
+Diogo de J. S. Machado
+"""
+
+import biotext as bt
+import re
+import pandas as pd
+
+# Set this to true if you want to re-run the pubmed search,
+# or keep it to False to use the previously downloaded set.
+reRun_pubmedSearch = False
+pubmedResult_fileName = 'pubmedResult_thioredoxin.tsv'
+tree_fileName = 'tree_thioredoxin.nwk'
+
+# Download or import pubmed result
+if reRun_pubmedSearch:
+    email = 'Your.Name.Here@example.org' # Email to inform pubmed who is doing the search.
+    search_string = 'thioredoxin'
+    
+    pubmed_set = bt.pubSearch(search_string,email,batch_size=1000)
+    pubmed_set.to_csv(pubmedResult_fileName,header=True,index=False,sep='\t')
+else:
+    pubmed_set = pd.read_csv(pubmedResult_fileName,sep='\t')
+
+# Run aminocode
+fasta = bt.aminocode.encodefile(pubmed_set['ti'],detailing='dp')
+
+# Run SWeeP
+mat = bt.fasta2vect(fasta)
+
+# Create a string to identify the dendrogram leaves, using the PMIDs and paper titles.
+# Non-alnumeric and non-space characters in titles are replaced with "_", to avoid conflict with the newick format syntax.
+leaves = [str(r['pmid'])+' - '+re.sub('[^\w\s]','_',r['ti']) for i,r in pubmed_set.iterrows()]
+
+# Run dendrogram (tree) creation in newick format
+tree = bt.mat2tree(mat,leaves,method='complete')
+
+# Save tree in a file
+with open(tree_fileName,'w') as f:
+    f.write(tree)
